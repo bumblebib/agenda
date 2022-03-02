a52f17d859a08ceea75f5dadfb8d8e246df78a9e
Bianca Ribeiro <biancaribeiro93036@gmail.com>
2022-03-02 11:09:17 -0300

    Criado tabela eventos para trabalhar com a agenda

--- agenda/settings.py
+++ agenda/settings.py
@@ -19,9 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-from decouple import config
-
-SECRET_KEY = config("SECRET_KEY")
+# SECURITY WARNING: keep the secret key used in production secret!
+# -> Secret Key aqui
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100) #cria campo de char com tamanho maximo de 100 caracteres
+    descricao = models.TextField(blank=True, null=True) #cria um campo de texto que pode estar vazio ou nulo
+    data_evento = models.DateTimeField(verbose_name='Data do Evento') #cria um campo de data que recebe a data do evento
+    data_criacao = models.DateTimeField(auto_now=True) #cria um campo de data, que tem a data de criação do evento
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #importa tabela usuarios do django e informando para excluir todos os eventos caso o usuario seja excluido
+
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo #retorna o nome do titulo no banco de dados
